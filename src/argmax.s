8eb835968c9370667bb887a3cdb750d03a39578e
jianjia <1208872131@qq.com>
2024-08-07 17:30:20 +0800

    task 3

--- src/argmax.s
+++ src/argmax.s
@@ -15,16 +15,43 @@
 #     this function terminates the program with error code 36
 # =================================================================
 argmax:
-    # Prologue
+  # Prologue
+  addi sp sp -20
+  sw s0 0(sp)
+  sw s1 4(sp)
+  sw s2 8(sp)
+  sw s3 12(sp)
+  sw ra 16(sp)
 
+  blt x0 a0 loop_start
+  addi a0 x0 36
+  j exit
 
 loop_start:
-
+  addi s0 x0 0
+  addi s1 x0 0
+  addi t0 a0 0
+  lw s2 0(t0)
 
 loop_continue:
-
+  lw s3 0(t0)
+  bge s2 s3 loop_end
+  add s2 x0 s3
+  add s0 x0 s1
 
 loop_end:
-    # Epilogue
+  addi s1 s1 1
+  addi t0 t0 4
+  blt s1 a1 loop_continue
 
-    jr ra
+  add a0 x0 s0
+
+  # Epilogue
+  lw s0 0(sp)
+  lw s1 4(sp)
+  lw s2 8(sp)
+  lw s3 12(sp)
+  lw ra 16(sp)
+  addi sp sp 20
+
+  jr ra
