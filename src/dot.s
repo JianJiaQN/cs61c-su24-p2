6dacf6053ecf201bf771d2076cadb5d42b52087c
jianjia <1208872131@qq.com>
2024-08-07 17:30:35 +0800

    task 4

--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,39 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
+  # Prologue
 
-    # Prologue
+  addi t0 x0 1
+  blt a3 t0 done
+  blt a4 t0 done
 
+  addi t2 x0 0
+  addi t3 x0 0
+  slli a3 a3 2
+  slli a4 a4 2
+
+  blt x0 a2 loop_start
+
+  li a0 36
+  j exit 
+
+done:
+  li a0 37
+  j exit
 
 loop_start:
+  lw t0 0(a0)
+  lw t1 0(a1)
+  mul t1 t0 t1
+  add t2 t2 t1
 
-
-
-
-
-
-
-
-
-
-
+  addi t3 t3 1
+  add a0 a0 a3
+  add a1 a1 a4
+  blt t3 a2 loop_start
 
 loop_end:
-
-
     # Epilogue
 
-
-    jr ra
+  mv a0 t2
+  jr ra
