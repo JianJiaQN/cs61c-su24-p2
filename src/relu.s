5bacc145e35ed48bcb7fa7adb252823959d1f28a
jianjia <1208872131@qq.com>
2024-08-07 17:30:09 +0800

    task 2

--- src/relu.s
+++ src/relu.s
@@ -13,26 +13,26 @@
 #     this function terminates the program with error code 36
 # ==============================================================================
 relu:
-    # Prologue
+  # Prologue
+  
+  addi t0 x0 0
+  addi t1 a0 0
+  addi t2 x0 1
+  bge a1 t2 loop_start
+  li a0 36
+  j exit
 
 
 loop_start:
-
-
-
-
-
-
-
+  lw t2 0(t1)
+  bge t2 x0 loop_continue
+  sw x0 0(t1)
 
 loop_continue:
-
-
-
+  addi t1 t1 4
+  addi t0 t0 1
+  blt t0 a1 loop_start
+  
 loop_end:
-
-
-    # Epilogue
-
-
-    jr ra
+  # Epilogue
+  jr ra
